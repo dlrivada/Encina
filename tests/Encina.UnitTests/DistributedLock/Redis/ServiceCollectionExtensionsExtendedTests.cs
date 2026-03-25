217aee190a483d009a5eb1758dd8d697743257bb
dlrivada <3762783+dlrivada@users.noreply.github.com>
2026-03-25 13:35:58 +0100

    test: recover DistributedLock.Redis, MQTT, Tenancy tests
    Recover 6 more deleted test files with proper namespace
    disambiguation (HealthStatus ambiguity, global:: prefixes).
    
    Files recovered:
    - DistributedLock/Redis/RedisDistributedLockHealthCheckTests.cs
    - DistributedLock/Redis/RedisDistributedLockProviderExtendedTests.cs
    - DistributedLock/Redis/ServiceCollectionExtensionsExtendedTests.cs
    - DistributedLock/SqlServer/SqlServerDistributedLockHealthCheckTests.cs
    - MQTT/Health/MQTTHealthCheckExtendedTests.cs
    - Tenancy/TenantHealthCheckTests.cs
    
    Key fix: HealthStatus enum has ambiguity between
    Microsoft.Extensions.Diagnostics.HealthChecks.HealthStatus and
    Encina.Messaging.Health.HealthStatus. Resolved with using alias
    and int cast for ShouldBe assertions.
    
    Refs #899, #900

---
+++ tests/Encina.UnitTests/DistributedLock/Redis/ServiceCollectionExtensionsExtendedTests.cs
@@ -0,0 +1,72 @@
+using Encina.DistributedLock;
+using Encina.DistributedLock.Redis;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
+using StackExchange.Redis;
+
+namespace Encina.UnitTests.DistributedLock.Redis;
+
+public class ServiceCollectionExtensionsExtendedTests
+{
+    [Fact]
+    public void AddEncinaDistributedLockRedis_NullServices_Throws()
+    {
+        Should.Throw<ArgumentNullException>(() =>
+            ((IServiceCollection)null!).AddEncinaDistributedLockRedis("localhost:6379"));
+    }
+
+    [Fact]
+    public void AddEncinaDistributedLockRedis_NullConnectionString_Throws()
+    {
+        var services = new ServiceCollection();
+        Should.Throw<ArgumentException>(() =>
+            services.AddEncinaDistributedLockRedis((string)null!));
+    }
+
+    [Fact]
+    public void AddEncinaDistributedLockRedis_WithMultiplexer_NullServices_Throws()
+    {
+        var mux = Substitute.For<IConnectionMultiplexer>();
+        Should.Throw<ArgumentNullException>(() =>
+            ((IServiceCollection)null!).AddEncinaDistributedLockRedis(mux));
+    }
+
+    [Fact]
+    public void AddEncinaDistributedLockRedis_WithMultiplexer_NullMux_Throws()
+    {
+        var services = new ServiceCollection();
+        Should.Throw<ArgumentNullException>(() =>
+            services.AddEncinaDistributedLockRedis((IConnectionMultiplexer)null!));
+    }
+
+    [Fact]
+    public void AddEncinaDistributedLockRedis_WithMultiplexer_RegistersProvider()
+    {
+        var mux = Substitute.For<IConnectionMultiplexer>();
+        var services = new ServiceCollection();
+
+        services.AddEncinaDistributedLockRedis(mux);
+
+        var sp = services.BuildServiceProvider();
+        sp.GetService<IDistributedLockProvider>().ShouldNotBeNull();
+    }
+
+    [Fact]
+    public void AddEncinaDistributedLockRedis_WithMultiplexerAndConfigure_NullConfigure_Throws()
+    {
+        var mux = Substitute.For<IConnectionMultiplexer>();
+        var services = new ServiceCollection();
+
+        Should.Throw<ArgumentNullException>(() =>
+            services.AddEncinaDistributedLockRedis(mux, null!));
+    }
+
+    [Fact]
+    public void AddEncinaDistributedLockRedis_ReturnsSameCollection()
+    {
+        var mux = Substitute.For<IConnectionMultiplexer>();
+        var services = new ServiceCollection();
+        var result = services.AddEncinaDistributedLockRedis(mux);
+        result.ShouldBeSameAs(services);
+    }
+}
